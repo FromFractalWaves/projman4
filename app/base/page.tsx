548fb198fd5d847fa47f18f7a5f9c5b6da0d8373
FromFractalWaves <ems910@outlook.com>
2025-02-15 14:03:58 -0500

    for dev testing

---
+++ app/base/page.tsx
@@ -0,0 +1,8 @@
+// app/page.tsx
+'use client';
+
+import { IntegratedDashboard } from '@/components/dashboard/IntegratedDashboard';
+
+export default function DashboardPage() {
+  return <IntegratedDashboard />;
+}
